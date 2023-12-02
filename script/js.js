ce54f8e8404de1a48a7e978f278290ed5e0952ff
Akhmetov Ruslan <45430415+Cabarnet@users.noreply.github.com>
2023-12-02 19:41:00 +0300

    Update js.js

--- script/js.js
+++ script/js.js
@@ -62,6 +62,7 @@ function showWeather(){
     if(data.weather[0].main == "Clear") $body.style.background = 'linear-gradient(150.64deg, #51a7ff 0%, #0058e0 100%)';
     if(data.weather[0].main == "Clouds") $body.style.background = 'linear-gradient(150.64deg, rgb(170 171 183) 0%, rgb(131 131 131) 100%)';
     if(data.weather[0].main == "Rain") $body.style.background = 'linear-gradient(150.64deg, rgb(83 112 179) 0%, rgb(3 43 215) 100%)';
+    if(data.weather[0].main == "Mist") $body.style.background = 'linear-gradient(150.64deg, #8bada4 0%, #36607f 100%)';
     if(data.weather[0].main == "Snow") 
     {
       $body.style.background = 'linear-gradient(150.64deg, rgb(255 255 255) 0%, rgb(241 241 241) 100%)';
